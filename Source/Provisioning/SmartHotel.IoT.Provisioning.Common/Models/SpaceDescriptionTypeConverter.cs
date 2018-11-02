b1e40e40ee492f4919f7cd6f4f0d0c67ba89682c
Travis Schilling <traviss@interknowlogy.com>
2018-11-02 09:20:51 -0700

    Finished updating the provisioning generator to use YamlDotNet serialization - need to get it so it stops outputting empty collections of things... partway through this with the SpaceDescriptionTypeConverter

---
+++ Source/Provisioning/SmartHotel.IoT.Provisioning.Common/Models/SpaceDescriptionTypeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace SmartHotel.IoT.Provisioning.Common.Models
+{
+    public class SpaceDescriptionTypeConverter : IYamlTypeConverter
+    {
+	    public bool Accepts(Type type)
+	    {
+		    return type == typeof(SpaceDescription);
+	    }
+
+	    public object ReadYaml(IParser parser, Type type)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public void WriteYaml(IEmitter emitter, object value, Type type)
+	    {
+		    if (!(value is SpaceDescription sd))
+		    {
+			    return;
+		    }
+
+		    emitter.Emit(new Scalar(nameof(SpaceDescription.name), sd.name));
+	    }
+    }
+}
